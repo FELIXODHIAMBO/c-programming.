a6b7e30133958293e77efe9c9c56c06f51ac5cbc
FELIXODHIAMBO <113968327+FELIXODHIAMBO@users.noreply.github.com>
2022-11-08 23:17:40 +0300

    Add files via upload

---
+++ Object program.cpp
@@ -0,0 +1,39 @@
+1. // class program
+
+2. #include <iostream>
+
+3. using namespace std;
+
+4. class addition{
+
+5. public:
+
+6. int a=22,b=30,c=50;
+
+7. int product(){
+
+8. return(a*b*c);
+
+9. }
+
+10. };
+
+11. int main() {
+
+12. addition addition1;
+
+13. int a,b,c,product,addition;
+
+14. a=addition1.a=22;
+
+15. b=addition1.b=30;
+
+16. c=addition1.c=50;
+
+17. product=addition1.a*b*c;
+
+18. cout<<"product is"<<product<<endl;
+
+19. return 0;
+
+20. }
